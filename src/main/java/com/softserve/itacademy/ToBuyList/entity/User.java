edca5b5fbc55cb4c65c107f019519ed0e446125d
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-01 00:18:39 +0300

    add entities

---
+++ src/main/java/com/softserve/itacademy/ToBuyList/entity/User.java
@@ -0,0 +1,78 @@
+package com.softserve.itacademy.ToBuyList.entity;
+
+import java.util.Objects;
+
+public class User {
+    private Integer id;
+    private String email;
+    private String password;
+    private String username;
+
+    public User() {
+    }
+
+    public User(Integer id, String email, String password, String username) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) &&
+                email.equals(user.email) &&
+                password.equals(user.password) &&
+                username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, username);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
