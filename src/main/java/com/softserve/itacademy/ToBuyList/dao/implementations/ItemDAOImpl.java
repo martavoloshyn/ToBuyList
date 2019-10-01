bd82c789430f5ebdf5d2484327508a8089635a66
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-01 10:20:49 +0300

    add ItemDAOImpl

---
+++ src/main/java/com/softserve/itacademy/ToBuyList/dao/implementations/ItemDAOImpl.java
@@ -0,0 +1,111 @@
+package com.softserve.itacademy.ToBuyList.dao.implementations;
+
+import com.softserve.itacademy.ToBuyList.dao.interfaces.ItemDAO;
+import com.softserve.itacademy.ToBuyList.entity.Item;
+import com.softserve.itacademy.ToBuyList.entity.List;
+import com.softserve.itacademy.ToBuyList.jdbc.DBConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ItemDAOImpl implements ItemDAO {
+    private Connection connection;
+
+    public ItemDAOImpl() {
+        connection = DBConnection.getConnection();
+    }
+    @Override
+    public ArrayList<Item> getItemsByList(Integer idList) throws SQLException {
+        String query = "SELECT * FROM item WHERE idlist=?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        return getItems(statement);
+    }
+
+    @Override
+    public ArrayList<Item> getDoneItemsByList(Integer idList) throws SQLException {
+        String query = "SELECT * FROM item WHERE idlist=? AND isdone=true";
+        PreparedStatement statement = connection.prepareStatement(query);
+        return getItems(statement);
+    }
+
+    private ArrayList<Item> getItems(PreparedStatement statement) throws SQLException {
+        ArrayList<Item> items = new ArrayList<>();
+        ResultSet set = statement.executeQuery();
+        while (set.next()) {
+            Item item = new Item();
+            item.setId(set.getInt("iditem"));
+            item.setIdList(set.getInt("idlist"));
+            item.setText(set.getString("text"));
+            item.setCreateDate(set.getDate("createdate").toLocalDate());
+            item.setUpdateDate(set.getDate("updatedate").toLocalDate());
+            items.add(item);
+        }
+        set.close();
+        statement.close();
+        return items;
+    }
+
+    @Override
+    public ArrayList<Item> getUndoneItemsByList(Integer idList) throws SQLException {
+        String query = "SELECT * FROM item WHERE idlist=? AND isdone=false";
+        PreparedStatement statement = connection.prepareStatement(query);
+        return getItems(statement);
+    }
+
+    @Override
+    public void add(Item object) throws SQLException {
+        String query = "INSERT INTO item (iditem, idlist, text, createdate,updatedate, isdone ) VALUES (?,?,?,?,?,?)";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1,object.getId());
+        statement.setInt(2,object.getIdList());
+        statement.setString(3, object.getText());
+        statement.setDate(4, Date.valueOf(object.getCreateDate()));
+        statement.setDate(5,Date.valueOf(object.getUpdateDate()));
+        statement.setBoolean(6,object.getDone());
+        statement.execute();
+        statement.close();
+    }
+
+    @Override
+    public Item get(Integer id) throws SQLException {
+        String query = "SELECT * FROM item WHERE iditem=?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1,id);
+        ResultSet set = statement.executeQuery();
+        Item item = new Item();
+        if (set.next()) {
+            item.setId(set.getInt("iditem"));
+            item.setIdList(set.getInt("idlist"));
+            item.setText(set.getString("name"));
+            item.setCreateDate(set.getDate("createdate").toLocalDate());
+            item.setUpdateDate(set.getDate("updatedate").toLocalDate());
+            item.setDone(set.getBoolean("isdone"));
+        }
+        set.close();
+        statement.close();
+        return item;
+    }
+
+    @Override
+    public void update(Item object) throws SQLException {
+        String query = "UPDATE item SET idlist=?, text=?, createdate=?, updatedate=?, isdone=?  WHERE iditem=?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1,object.getIdList());
+        statement.setString(2, object.getText());
+        statement.setDate(3,Date.valueOf(object.getCreateDate()));
+        statement.setDate(4,Date.valueOf(object.getUpdateDate()));
+        statement.setBoolean(5,object.getDone());
+        statement.setInt(6,object.getId());
+        statement.execute();
+        statement.close();
+    }
+
+    @Override
+    public void delete(Integer id) throws SQLException {
+        String query = "DELETE FROM item WHERE id=?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1,id);
+        statement.execute();
+        statement.close();
+    }
+}
