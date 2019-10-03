576a909630218397891f65dd9923573c415581fa
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-03 12:07:06 +0300

    add user service

---
+++ src/main/java/com/softserve/itacademy/ToBuyList/service/interfaces/UserService.java
@@ -0,0 +1,14 @@
+package com.softserve.itacademy.ToBuyList.service.interfaces;
+
+import com.softserve.itacademy.ToBuyList.entity.User;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface UserService extends Service<User>{
+    User getUserByEmail(String email);
+
+    ArrayList<String> getAllEmails();
+
+    ArrayList<String> getAllUsernames();
+}
