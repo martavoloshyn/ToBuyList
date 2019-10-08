415e72db5905f07f0783d57337b81a3b35028c09
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-08 15:20:33 +0300

    add checkbox for items

---
+++ src/main/java/com/softserve/itacademy/ToBuyList/controller/ChangeDoneServlet.java
@@ -0,0 +1,22 @@
+package com.softserve.itacademy.ToBuyList.controller;
+
+import com.softserve.itacademy.ToBuyList.service.implementations.ItemServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/changeDone")
+public class ChangeDoneServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ItemServiceImpl itemService = new ItemServiceImpl();
+        Integer idItem = Integer.parseInt(req.getParameter("idItem"));
+        System.out.println(idItem);
+        itemService.changeDoneById(idItem);
+
+    }
+}
